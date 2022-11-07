8b0edc0e57cc0ea4ee698556b031a144511a0e9f
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-08 01:28:05 +0600

    navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  }
+])
 
 function App() {
   return (
-    <div>
-      <h1>
-        Mesuk
-      </h1>
+    <div className='lg:w-4/5 mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
